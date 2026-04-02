0a7b3369e3bc7a742120b6d94f32b74abcd605a3
Ed Page <eopage@gmail.com>
2026-04-02 10:35:59 -0500

    feat(cli): Expand capabilities

--- battery-packs/cli-battery-pack/templates/simple/src/main.rs
+++ battery-packs/cli-battery-pack/templates/simple/src/main.rs
@@ -1,3 +1,6 @@
+use anstream::println;
+use anstyle::AnsiColor;
+use anstyle_hyperlink::Hyperlink;
 use clap::Parser;
 use tracing::info;
 
@@ -9,19 +12,41 @@ struct Cli {
     #[arg(short, long, default_value = "World")]
     name: String,
 
+    #[command(flatten)]
+    color: colorchoice_clap::Color,
+
     /// Enable verbose output
     #[arg(short, long)]
     verbose: bool,
 }
 
 fn main() -> anyhow::Result<()> {
-    let cli = Cli::parse();
+    human_panic::setup_panic!();
+
+    let cli = Cli::parse_from(wild::args());
+
+    cli.color.write_global();
 
     if cli.verbose {
         tracing_subscriber::fmt::init();
     }
 
+    let supports_hyperlinks = supports_hyperlinks::supports_hyperlinks();
+
     info!("Starting {{ project_name }}");
-    println!("Hello, {}!", cli.name);
+    let name_link = supports_hyperlinks
+        .then(|| Hyperlink::with_url(format!("https://crates.io/crates/{}", cli.name)))
+        .unwrap_or_default();
+    let name_color = AnsiColor::Green.on_default();
+    println!(
+        "Hello, {name_link}{name_color}{}{name_color:#}{name_link:#}!",
+        cli.name
+    );
     Ok(())
+}
+
+#[test]
+fn verify_cli() {
+    use clap::CommandFactory;
+    Cli::command().debug_assert();
 }
